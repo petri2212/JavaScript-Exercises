be819e6db8c4be0ab3ddab13716e52adb26f73e8
Andrei <a.petrisor@studenti.unibg.it>
2025-04-19 14:02:40 +0200

    Changing the html using js (DOM)

--- counter_people_app/index.js
+++ counter_people_app/index.js
@@ -1,13 +1,21 @@
 // document.getElementById("count-el").innerText = 5;
 // code runner ---> ctrl+alt+N for run
-
+let countEl = document.getElementById("count-el")
+let savedEl = document.getElementById("save-el")
 let count = 0;
 
 function increment(){
-    count = count + 1
-    console.log(count) 
+    count += 1
+    countEl.textContent = count
+  //  console.log(count) 
 }
 
-
+function save(){
+    let countStr = count + " - "
+    savedEl.textContent += countStr //i use .textContent because .innerText loose the second space in " - "
+    countEl.textContent = 0
+    count = 0
+  //  console.log(count) 
+}
 
 
