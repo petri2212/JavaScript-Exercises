0c4a8103f273699fa05a36f9119fed834ee4401d
Andrei <a.petrisor@studenti.unibg.it>
2025-05-02 18:52:55 +0200

    final version released

--- chrome-extension(save urls)/index.js
+++ chrome-extension(save urls)/index.js
@@ -1,14 +1,52 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 //const cant be riassigned, so inputEl = "hello" this dont work
+//localStorage.clear()
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+//localStorage.setItem("myLeads",  "www.examplead.com")
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+tabBtn.addEventListener("click", function(){
+    //fa la query a chrome che mi da la tab attiva tra quelle della pragina, currentWindow:true quella più alta di layer
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        //quando trova la tab fa la funzione
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads()
+    })
+
+    
+})
+
+//you have to double click
+deleteBtn.addEventListener("dblclick", function () {
+
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+
+})
 
 inputBtn.addEventListener("click", function () {
+
+
+
     myLeads.push(inputEl.value);
     inputEl.value = ""
-    console.log(myLeads);
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
     renderLeads()
+
+
 })
 
 function renderLeads() {
